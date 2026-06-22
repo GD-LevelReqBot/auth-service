b32bfa838b65bac40fe238f026abde7c4739e0e0
SuperDev <supernova@superdev.one>
2026-06-21 22:04:09 -0500

    fix a few issues

--- src/strategies/twitch.js
+++ src/strategies/twitch.js
@@ -2,19 +2,30 @@ const passport = require('passport');
 const { OAuth2Strategy } = require('passport-oauth');
 const config = require('../config');
 
-passport.use('twitch', new OAuth2Strategy(
+const strategy = new OAuth2Strategy(
     {
         authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
-        tokenURL: 'https://id.twitch.tv/oauth2/token',
-        clientID: config.twitch.clientId,
-        clientSecret: config.twitch.clientSecret,
-        callbackURL: config.twitch.redirectUri,
-        state: true, // passport-oauth generates + verifies the state parameter
+        tokenURL:         'https://id.twitch.tv/oauth2/token',
+        clientID:         config.twitch.clientId,
+        clientSecret:     config.twitch.clientSecret,
+        callbackURL:      config.twitch.redirectUri,
+        state:            true,
     },
     (accessToken, refreshToken, profile, done) => {
         done(null, { provider: 'twitch', accessToken, refreshToken });
     }
-));
+);
 
+// passport-oauth2's base authorizationParams() returns {} and drops unknown
+// options. Override it so Twitch-specific params actually reach the URL.
+strategy.authorizationParams = function (options) {
+    const params = {};
+    // force_verify=true makes Twitch always show the authorization screen,
+    // even if the user previously granted all requested scopes.
+    if (options.force_verify) params.force_verify = 'true';
+    return params;
+};
+
+passport.use('twitch', strategy);
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
